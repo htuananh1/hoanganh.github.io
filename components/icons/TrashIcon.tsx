7926640e27d4ecf9981cfba1e1ea32182e0c75f2
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2025-09-30 13:53:56 +0700

    Update TrashIcon.tsx

--- components/icons/TrashIcon.tsx
+++ components/icons/TrashIcon.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 
-interface TrashIconProps {
-  className?: string;
-}
-
-const TrashIcon: React.FC<TrashIconProps> = ({ className = '' }) => {
-  return (
-    <svg
-      className={className}
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
+export const TrashIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        className={className}
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-      />
+        <path d="M3 6h18"/>
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+        <line x1="10" y1="11" x2="10" y2="17"/>
+        <line x1="14" y1="11" x2="14" y2="17"/>
     </svg>
-  );
-};
-
-export { TrashIcon };
+);
